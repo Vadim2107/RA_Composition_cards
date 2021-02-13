475bbdf10df032c4917e5335c7629177364b50ac
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-02-13 16:33:36 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Card from './components/Card';
+import img from './img/transmission.jpg'
 
 class App extends React.Component {
   
@@ -8,13 +9,10 @@ class App extends React.Component {
     
     return (
       <React.Fragment>
-        <Card>
-          <img src="https://cdn2.hexlet.io/derivations/image/fill/850/400/eyJpZCI6IjhkZjFkY2M5MTU1N2NkYWY4NzY4OWQxNmJhNzYzOThmLmpwZWciLCJzdG9yYWdlIjoic3RvcmUifQ?signature=85aa61960604801d554d75746a971a269c73ecd543cd1aa07187350d5886aed6" className="card-img-top" alt="..."></img>
-          <h3 className="card-title">Card title</h3>
+        <Card img={img} title='Card title'>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </Card>
-        <Card>
-          <h3 className="card-title">Special title treatment</h3>
+        <Card title='Special title treatment'>
           <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
         </Card>
       </React.Fragment>                
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Card = (props) => {
-
+    console.log(props.children);
     return (
         <div className="card">
             <div className="card-body">
+                <img src={props.img} className="card-img-top" alt="..."></img>
+                <h3 className="card-title">{props.title}</h3>
                 {props.children}
                 <div className="btn">
                     <a href="#0" className="btn-primary">Go somewhere</a>
